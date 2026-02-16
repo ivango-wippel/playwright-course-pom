03697eab3bfdc81990beb2c935e2f67cae859846
ivango <ognavi@gmail.com>
2026-02-16 07:01:53 -0300

    Primeiro commit

---
+++ conftest.py
@@ -0,0 +1,12 @@
+import pytest
+from pages.login_page import LoginPage
+
+@pytest.fixture
+def page(page):
+    page.goto("https://leogcarvalho.github.io/simulabank/login.html")
+    return page
+
+@pytest.fixture
+def login_page(page):
+    return LoginPage(page)
+
